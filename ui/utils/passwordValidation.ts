4b3a7fcf31f5e3a1d0139f18ff947d7386e1bfb1
Artem Burakov <artem@burakov.com>
2024-04-18 11:44:26 +0300

    updates regex

--- ui/utils/passwordValidation.ts
+++ ui/utils/passwordValidation.ts
@@ -3,7 +3,7 @@ import i18next from "i18next"
 const MIN_PASSWORD_LENGTH = 8
 const MAX_PASSWORD_LENGTH = 20
 
-const PASSWORD_REGEX = /^[A-Za-z0-9]+$/
+const PASSWORD_REGEX = /^[A-Za-z0-9!"#$%&'()*+,\-.\/:;<=>?@[\\\]^_`{|}~]+$/
 
 export const validatePassword = (
   password: string,

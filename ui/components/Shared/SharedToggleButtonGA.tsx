136dc441337f1aa1315b2fef6cbbd9c73ab141ea
Artem Burakov <artem@burakov.com>
2024-05-01 18:51:18 +0300

    GA-feat: adds tooltip to disabled toggle switch

--- ui/components/Shared/SharedToggleButtonGA.tsx
+++ ui/components/Shared/SharedToggleButtonGA.tsx
@@ -1,5 +1,7 @@
 import React, { useState, ReactElement, useEffect } from "react"
 import classNames from "classnames"
+import SharedTooltip from "./SharedTooltip"
+import { useTranslation } from "react-i18next"
 
 type SharedToggleButtonGAProps = {
   onChange: (toggleValue: boolean) => void
@@ -19,6 +21,10 @@ export default function SharedToggleButtonGA({
   leftToRight = true,
   isDisabled = false,
 }: SharedToggleButtonGAProps): ReactElement {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "drawers.selectNetwork",
+  })
+
   const [isActive, setIsActive] = useState(value || false)
 
   useEffect(() => setIsActive(!!value), [value])
@@ -26,54 +32,65 @@ export default function SharedToggleButtonGA({
   const handleToggleAction = () => onChange(!isActive)
 
   return (
-    <button
-      type="button"
-      role="checkbox"
-      aria-checked={isActive}
-      disabled={isDisabled}
-      className={classNames("container", {
-        is_active: isActive,
-        disabled: isDisabled,
-      })}
-      onClick={handleToggleAction}
-    >
-      <div className="bulb" />
-      <style jsx>
-        {`
-          .container {
-            width: 28px;
-            height: 18px;
-            border-radius: 20px;
-            background-color: var(--green-120);
-            box-sizing: border-box;
-            padding: 1px;
-            cursor: pointer;
-            display: flex;
-            border: 1px solid var(--hunter-green)
-              ${leftToRight ? "" : "transform: rotate(180deg);"};
-          }
-          .bulb {
-            width: 14px;
-            height: 14px;
-            border-radius: 20px;
-            background-color: var(--hunter-green);
-            transition: 0.2s ease-in-out;
+    <SharedTooltip
+      type="dark"
+      width={200}
+      height={35}
+      verticalPosition="top"
+      horizontalPosition="left"
+      horizontalShift={30}
+      IconComponent={() => (
+        <button
+          type="button"
+          role="checkbox"
+          aria-checked={isActive}
+          disabled={isDisabled}
+          className={classNames("container", {
+            is_active: isActive,
+            disabled: isDisabled,
+          })}
+          onClick={handleToggleAction}
+        >
+          <div className="bulb" />
+          <style jsx>
+            {`
+              .container {
+                width: 28px;
+                height: 18px;
+                border-radius: 20px;
+                background-color: var(--green-120);
+                box-sizing: border-box;
+                padding: 1px;
+                cursor: pointer;
+                display: flex;
+                border: 1px solid var(--hunter-green)
+                  ${leftToRight ? "" : "transform: rotate(180deg);"};
+              }
+              .bulb {
+                width: 14px;
+                height: 14px;
+                border-radius: 20px;
+                background-color: var(--hunter-green);
+                transition: 0.2s ease-in-out;
 
-            box-shadow: 0px 1px 1px rgba(0, 20, 19, 0.3);
-          }
-          .is_active .bulb {
-            transform: translateX(10px);
-          }
-          .is_active {
-            background-color: var(--green-80);
-          }
-          .disabled {
-            opacity: 0.5;
-            cursor: not-allowed;
-            background-color: var(--disabled);
-          }
-        `}
-      </style>
-    </button>
+                box-shadow: 0px 1px 1px rgba(0, 20, 19, 0.3);
+              }
+              .is_active .bulb {
+                transform: translateX(10px);
+              }
+              .is_active {
+                background-color: var(--green-80);
+              }
+              .disabled {
+                opacity: 0.5;
+                background-color: var(--disabled);
+              }
+            `}
+          </style>
+        </button>
+      )}
+    >
+      <div className="centered_tooltip">{t("disabledToggleMessage")}</div>
+    </SharedTooltip>
   )
 }

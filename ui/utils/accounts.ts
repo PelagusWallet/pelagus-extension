7ee80d5540c709f091778bcf8a8137042258f54a
MichaelKostroma <mykhailkostroma@gmail.com>
2024-05-21 11:18:50 +0300

    GA-refactor: change account details font size and improve serach accounts functionality

--- ui/utils/accounts.ts
+++ ui/utils/accounts.ts
@@ -1,6 +1,29 @@
 import { AccountType } from "@pelagus/pelagus-background/redux-slices/accounts"
+import { AccountTotal } from "@pelagus/pelagus-background/redux-slices/selectors"
 
 export const isAccountWithSecrets = (accountType: AccountType): boolean =>
   accountType === AccountType.Imported ||
   accountType === AccountType.Internal ||
   accountType === AccountType.PrivateKey
+
+export const searchAccountsHandle = (
+  searchValue: string,
+  accounts: AccountTotal[]
+): AccountTotal[] => {
+  if (!searchValue) return accounts
+
+  const searchValueToLowerCase = searchValue.toLowerCase()
+  return accounts?.filter((item: AccountTotal) => {
+    const name = item.name?.toLowerCase()
+    const address = item.address?.toLowerCase()
+    const shard =
+      item?.accountSigner?.type === "keyring" &&
+      item.accountSigner?.shard?.toLowerCase()
+
+    return (
+      name?.includes(searchValueToLowerCase) ||
+      address?.includes(searchValueToLowerCase) ||
+      (shard && shard.includes(searchValueToLowerCase))
+    )
+  })
+}

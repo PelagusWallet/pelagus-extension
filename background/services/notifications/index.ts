7aee8ade2937a9268ed34d9cb2b19e6ab4f0ee15
MichaelKostroma <mykhailkostroma@gmail.com>
2024-09-05 13:26:46 +0300

    feat: add redirecting on quaiscan

--- background/services/notifications/index.ts
+++ background/services/notifications/index.ts
@@ -1,25 +1,48 @@
 import { TemplateType } from "./types"
+import { QUAI_SCAN_URL } from "../../constants"
 
-export default class NotificationsManager {
-  public createSuccessTxNotification(nonce: number | null | undefined): void {
+export default abstract class NotificationsManager {
+  public static createSuccessTxNotification(
+    nonce: number | null | undefined,
+    txHash: string | undefined
+  ): void {
     const options = {
       type: TemplateType.basic,
       iconUrl: "../../icon-128.png",
       title: "Confirmed transaction",
-      message: `Transaction ${nonce} confirmed! View on QuaiScan`,
+      message: `Transaction ${nonce ?? 0} confirmed! View on QuaiScan`,
     }
 
-    chrome.notifications.create(options)
+    chrome.notifications.create(options, (notificationId) => {
+      chrome.notifications.onClicked.addListener((listenerId) => {
+        if (listenerId !== notificationId) return
+
+        chrome.tabs.create({
+          url: txHash ? `${QUAI_SCAN_URL}/tx/${txHash}` : QUAI_SCAN_URL,
+        })
+      })
+    })
   }
 
-  public createFailedTxNotification(nonce: number | null | undefined): void {
+  public static createFailedTxNotification(
+    nonce: number | null | undefined,
+    txHash: string | undefined
+  ): void {
     const options = {
       type: TemplateType.basic,
       iconUrl: "../../icon-128.png",
       title: "Failed transaction",
-      message: `Transaction ${nonce} failed! View on QuaiScan`,
+      message: `Transaction ${nonce ?? 0} failed! View on QuaiScan`,
     }
 
-    chrome.notifications.create(options)
+    chrome.notifications.create(options, (notificationId) => {
+      chrome.notifications.onClicked.addListener((listenerId) => {
+        if (listenerId !== notificationId) return
+
+        chrome.tabs.create({
+          url: txHash ? `${QUAI_SCAN_URL}/tx/${txHash}` : QUAI_SCAN_URL,
+        })
+      })
+    })
   }
 }

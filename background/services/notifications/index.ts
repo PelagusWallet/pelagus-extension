8c3c34ea63a1c2397e71dc17c7f49654cd3e426b
MichaelKostroma <mykhailkostroma@gmail.com>
2024-09-05 16:28:42 +0300

    feat: adds isLocked condition for createIncomingAssetsNotification

--- background/services/notifications/index.ts
+++ background/services/notifications/index.ts
@@ -25,28 +25,6 @@ export default abstract class NotificationsManager {
     })
   }
 
-  public static createFailedTxNotification(
-    nonce: number | null | undefined,
-    txHash: string | undefined
-  ): void {
-    const options = {
-      type: TemplateType.basic,
-      iconUrl: "../../icon-128.png",
-      title: "Failed transaction",
-      message: `Transaction ${nonce ?? 0} failed! View on QuaiScan`,
-    }
-
-    chrome.notifications.create(options, (notificationId) => {
-      chrome.notifications.onClicked.addListener((listenerId) => {
-        if (listenerId !== notificationId) return
-
-        chrome.tabs.create({
-          url: txHash ? `${QUAI_SCAN_URL}/tx/${txHash}` : QUAI_SCAN_URL,
-        })
-      })
-    })
-  }
-
   public static createIncomingAssetsNotification(
     amount: string,
     symbol: string,

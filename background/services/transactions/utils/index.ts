0b416be81c772f9ceac1839b3e5ba5dd554cac10
Artem Burakov <artem@burakov.com>
2024-09-18 18:08:46 +0300

    enhances transaction service with added comments and minor updates

--- background/services/transactions/utils/index.ts
+++ background/services/transactions/utils/index.ts
@@ -1,38 +1,7 @@
-import {
-  QuaiTransactionRequest,
-  QuaiTransactionResponse,
-} from "quais/lib/commonjs/providers"
-import { QuaiTransaction, TransactionReceipt } from "quais"
+import { QuaiTransactionResponse } from "quais/lib/commonjs/providers"
 
 import { QuaiTransactionDB } from "../types"
 import { QuaiTransactionStatus } from "../../chain/types"
-
-export const quaiTransactionFromRequest = (
-  request: QuaiTransactionRequest | QuaiTransaction,
-  status: QuaiTransactionStatus
-): QuaiTransactionDB => {
-  return {
-    to: request.to?.toString() ?? "",
-    from: request?.from ? request?.from.toString() : "",
-    chainId: Number(request.chainId) ?? 0,
-    hash: crypto.randomUUID(), // TODO: NEED TO CONFIRM THIS (TEMPORARY)
-    data: request.data ?? null,
-    gasLimit: null,
-    maxFeePerGas: null,
-    maxPriorityFeePerGas: null,
-    nonce: request.nonce ?? null,
-    status,
-    type: null,
-    value: request?.value ?? null,
-    index: BigInt(0),
-    blockHash: null,
-    blockNumber: null,
-    gasPrice: null,
-    gasUsed: null,
-    etxs: [],
-    logs: [],
-  }
-}
 
 export const quaiTransactionFromResponse = (
   transactionResponse: QuaiTransactionResponse,
@@ -58,22 +27,5 @@ export const quaiTransactionFromResponse = (
     gasUsed: null,
     etxs: [],
     logs: [],
-  }
-}
-
-export const quaiTransactionFromReceipt = (
-  transactionResponse: QuaiTransactionDB,
-  receipt: TransactionReceipt,
-  status: QuaiTransactionStatus
-): QuaiTransactionDB => {
-  return {
-    ...transactionResponse,
-    status,
-    blockHash: receipt.blockHash,
-    blockNumber: receipt.blockNumber,
-    gasPrice: receipt.gasPrice,
-    gasUsed: receipt.gasUsed,
-    etxs: [...receipt.etxs],
-    logs: [...receipt.logs],
   }
 }

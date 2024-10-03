a91c9a938293e684c331a2a7fc201fdc2131ca5f
Artem Burakov <artem@burakov.com>
2024-10-03 14:16:30 +0300

    adds mailbox contract into transactions service

---
+++ background/contracts/payment-channel-mailbox.ts
@@ -0,0 +1,23 @@
+import { EventFragment, FunctionFragment, Interface, InterfaceAbi } from "quais"
+
+export const MAILBOX_EVENTS = {
+  NotificationSent: EventFragment.from(
+    "NotificationSent(string senderPaymentCode, string receiverPaymentCode)"
+  ),
+}
+
+const MAILBOX_FUNCTIONS = {
+  register: FunctionFragment.from("register(string paymentCode)"),
+  notify: FunctionFragment.from(
+    "notify(string senderPaymentCode, string receiverPaymentCode)"
+  ),
+  getNotifications: FunctionFragment.from(
+    "getNotifications(string receiverPaymentCode) view returns (string[])"
+  ),
+}
+
+export const MAILBOX_ABI: InterfaceAbi = [
+  ...Object.values(MAILBOX_FUNCTIONS),
+  ...Object.values(MAILBOX_EVENTS),
+]
+export const MAILBOX_INTERFACE: Interface = new Interface(MAILBOX_ABI)

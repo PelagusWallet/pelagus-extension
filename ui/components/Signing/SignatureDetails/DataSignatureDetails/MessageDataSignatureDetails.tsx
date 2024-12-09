84cf74b36b52024db96514268de25bd6ecafa087
rileystephens28 <riley.stephens28@gmail.com>
2024-12-09 13:46:44 -0600

    Add support for EIP4361 signing requests

--- ui/components/Signing/SignatureDetails/DataSignatureDetails/MessageDataSignatureDetails.tsx
+++ ui/components/Signing/SignatureDetails/DataSignatureDetails/MessageDataSignatureDetails.tsx
@@ -3,6 +3,7 @@ import { MessageSigningRequest } from "@pelagus/pelagus-background/utils/signing
 import React, { ReactElement } from "react"
 import EIP191Info from "../../../SignData/EIP191Info"
 import DataSignatureDetails from "."
+import EIP4361Info from "../../../SignData/EIP4361Info"
 
 export type MessageDataSignatureDetailsProps = {
   messageRequest: MessageSigningRequest
@@ -21,6 +22,15 @@ export default function MessageDataSignatureDetails({
             excludeHeader
             signingData={messageRequest.signingData}
           />
+        </DataSignatureDetails>
+      )
+      case "eip4361":
+      return (
+        <DataSignatureDetails
+          requestingSource={messageRequest.signingData.domain}
+          excludeTitle
+        >
+          <EIP4361Info signingData={messageRequest.signingData} />
         </DataSignatureDetails>
       )
     default:
